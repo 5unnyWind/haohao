4f12dcc69f8e44bdff764e4c51d0a59f4a2cca8e
sunnywind <sunny-wind@foxmail.com>
2023-12-26 01:53:57 +0800

    2

--- src/App.tsx
+++ src/App.tsx
@@ -13,34 +13,93 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "./components/ui/popover";
+import { Separator } from "./components/ui/separator";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./components/ui/carousel";
 
 interface Props {}
 
 const App: FC<Props> = () => {
   return (
-    <div className="flex flex-col h-screen items-center justify-center">
+    <div className="flex flex-col min-h-screen items-center justify-center p-10">
       <Card>
         <CardHeader>
+          <div className="bg-[#878F88] w-64 h-36 rounded-lg overflow-hidden mb-4 ">
+            <img
+              src="https://images-1305580547.cos.ap-guangzhou.myqcloud.com/IMG_4086%20%281%29.jpg"
+              alt=""
+            />
+          </div>
+          <Separator />
+          <div className="mt-4"></div>
           <CardTitle> 号号🐹</CardTitle>
-          <CardDescription>一只紫仓（intp</CardDescription>
+          <CardDescription>
+            <p>一只紫仓（intp</p>
+            <p>爱吃饭在减肥 白天睡觉</p>
+          </CardDescription>
         </CardHeader>
         <CardContent>
-          <p>生日 2023.10.05</p>
-          <p>性别 弟弟</p>
+          <p>性别 爹</p>
+          <p>籍贯 上海</p>
           <p>星座 天秤座 ⚖️</p>
+          <p>生日 2023.10.05</p>
         </CardContent>
         <CardFooter>
           <Popover>
             <PopoverTrigger>
               <Badge variant="outline">🦊 @wj</Badge>
             </PopoverTrigger>
-            <PopoverContent>找我：CowboyBebopSpike-</PopoverContent>
+            <PopoverContent className="p-2">
+              找我：CowboyBebopSpike-
+            </PopoverContent>
           </Popover>
+          <Separator orientation="vertical" />
           <Popover>
             <PopoverTrigger>
               <Badge variant="outline">@听枫 🍨</Badge>
             </PopoverTrigger>
-            <PopoverContent>联系我：qtf174</PopoverContent>
+            <PopoverContent className="p-2">联系我：qtf174</PopoverContent>
+          </Popover>
+        </CardFooter>
+      </Card>
+      <div className="mt-10"></div>
+      <Card>
+        <CardHeader>
+          <div className="bg-[#878F88] w-64 h-64 rounded-lg overflow-hidden mb-4 ">
+            <img
+              src="https://images-1305580547.cos.ap-guangzhou.myqcloud.com/IMG_4088%20%281%29.jpg"
+              alt=""
+            />
+          </div>
+          <Separator />
+          <div className="mt-4"></div>
+          <CardTitle> 粉猴子🐒</CardTitle>
+          <CardDescription>一条六角恐龙（好好的呆呆邻居</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>性别 ？？？</p>
+          <p>籍贯 ？？？</p>
+          <p>星座 ？？？</p>
+          <p>生日 ？？？</p>
+        </CardContent>
+        <CardFooter>
+          <Popover>
+            <PopoverTrigger>
+              <Badge variant="outline">@小岛</Badge>
+            </PopoverTrigger>
+            <PopoverContent className="p-2">找我：nutssea0401</PopoverContent>
+          </Popover>
+          <Separator orientation="vertical" />
+          <Popover>
+            <PopoverTrigger>
+              <Badge variant="outline">@qbq</Badge>
+            </PopoverTrigger>
+            <PopoverContent className="p-2">联系我：？？？</PopoverContent>
           </Popover>
         </CardFooter>
       </Card>
